cec6f5909b999c20af22ef7f5117823554e2d4ef
Luiz Sandoval (Americas) <luiz.sandoval@global.ntt>
2021-12-18 00:26:45 -0300

    chore: setup both miragejs and axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+
+import { api } from '../../services/api';
+
 import { Container } from './styles';
 
 export function TransactionsTable() {
+    useEffect(() => {
+        api.get('/transactions').then((response) => console.log(response.data));
+    }, []);
+
     return (
         <Container>
             <table>
@@ -15,13 +23,13 @@ export function TransactionsTable() {
                 <tbody>
                     <tr>
                         <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000</td>
+                        <td className='deposit'>R$ 12.000</td>
                         <td>Desenvolvimento</td>
                         <td>18/12/2021</td>
                     </tr>
                     <tr>
                         <td>Aluguel</td>
-                        <td className="withdraw">R$ -1.100</td>
+                        <td className='withdraw'>R$ -1.100</td>
                         <td>Casa</td>
                         <td>17/12/2021</td>
                     </tr>
