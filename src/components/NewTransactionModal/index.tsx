a014b92698480bf59a58fec59875a6ae89086ae3
Luiz Sandoval (Americas) <luiz.sandoval@global.ntt>
2021-12-22 21:14:49 -0300

    chore: adds transaction type buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import ReactModal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -31,6 +33,17 @@ export function NewTransactionModal({
                 <h2>Cadastrar transação</h2>
                 <input placeholder='Título' />
                 <input type='number' placeholder='Valor' />
+
+                <TransactionTypeContainer>
+                    <button type='button'>
+                        <img src={incomeImg} alt='Entrada' />
+                        <span>Entrada</span>
+                    </button>
+                    <button type='button'>
+                        <img src={outcomeImg} alt='' />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
                 <input placeholder='Categoria' />
                 <button type='submit'>Cadastrar</button>
             </Container>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -29,7 +30,7 @@ export const Container = styled.form`
         }
     }
 
-    button[type="submit"] {
+    button[type='submit'] {
         width: 100%;
         height: 4rem;
         padding: 0 1.5rem;
@@ -37,10 +38,10 @@ export const Container = styled.form`
 
         color: #fff;
         background: var(--green);
-        
+
         border: 0;
         border-radius: 0.25rem;
-        
+
         font-size: 1rem;
         font-weight: 600;
 
@@ -48,6 +49,48 @@ export const Container = styled.form`
 
         &:hover {
             filter: brightness(0.9);
+        }
+    }
+`;
+
+export const TransactionTypeContainer = styled.div`
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: 1fr 1fr;
+
+    margin: 1rem 0;
+
+    button {
+        height: 4rem;
+
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        transition: border-color 0.2s;
+
+        &:hover {
+            border-color: ${darken(0.1, '#d7d7d7')};
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+
+            color: var(--text-title);
+
+            font-size: 1rem;
+
+            margin-left: 1rem;
         }
     }
 `;
